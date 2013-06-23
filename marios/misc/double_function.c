a0b74c1621bc3f98572a209e771bb7f95dd3ffa7
Marios Pomonis <mariuska@ubuntu.(none)>
2013-06-23 16:39:15 -0700

    Added folder for miscellaneous test cases and double function test. Usage of temp.sh is ./temp.sh [filename_without_extension]

---
+++ marios/misc/double_function.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+void bar(int a)
+{
+	char b = (char) a;
+	printf("%c\n", b);
+}
+
+void foo1(int a)
+{
+	bar(a);
+}
+
+void foo(int a)
+{
+	foo1(a);
+}
+
+int main()
+{
+	char s[4];
+	fgets(s, 4, stdin);
+	int a = atoi(s);
+	foo(a);
+	printf("%d\n", a);
+	return 0;
+}
